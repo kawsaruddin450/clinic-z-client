4d2bd2ab8275c3a749d9014445ecb220a60948e9
kawsar-uddin <kawsaruddin450@gmail.com>
2024-08-15 01:24:38 +0600

    send checkout dat to backend

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -6,12 +6,44 @@ const Checkout = () => {
     const service = useLoaderData();
     console.log(service)
     const { user } = useContext(AuthContext);
+
+    const handleCheckout = event => {
+        event.preventDefault();
+        const form = event.target;
+
+        const username = form.username.value;
+        const email = form.email.value;
+        const phone = form.phone.vale;
+
+        const booking = {
+            username,
+            email,
+            phone,
+            title: service.namae,
+            description: service.description,
+            image: service.image,
+            price: service.price,
+            duration: service.duration,
+            availability: service.availability,
+        }
+        fetch('http://localhost:5000/bookings', {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(booking)
+        })
+        .then(res=> res.json())
+        .then( data => {
+            console.log(data);
+        })
+    }
     return (
         <div className="hero bg-base-200 min-h-screen lg:container mx-auto">
             <div className="w-3/4 lg:w-1/2">
                 <h1 className=" my-12 text-4xl font-bold text-center">You have selected: {service.name}</h1>
                 <div className="bg-base-100 shadow-2xl">
-                    <form className="card-body">
+                    <form onSubmit={handleCheckout} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Name</span>
@@ -31,7 +63,7 @@ const Checkout = () => {
                             <input type="text" placeholder="Phone number" name="phone" className="input input-bordered" required />
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-primary">Checkout</button>
+                            <input type="submit" className="btn btn-primary" value='Checkout'/>
                         </div>
                     </form>
                 </div>

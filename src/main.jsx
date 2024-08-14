0fdf854ec784f28d640df7153c429310fa964010
kawsar-uddin <kawsaruddin450@gmail.com>
2024-08-14 23:29:38 +0600

    basic route setup and implement header

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import {
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import router from './routes/routes.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
